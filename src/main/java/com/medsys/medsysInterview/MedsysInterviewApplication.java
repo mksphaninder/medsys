48087b20defa3228e6913f29727d4ca907cde60c
krishna <mksphaninder@gmail.com>
2019-03-30 12:36:01 -0400

    Initialized the project from spring boot starter

---
+++ src/main/java/com/medsys/medsysInterview/MedsysInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.medsys.medsysInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsysInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsysInterviewApplication.class, args);
+	}
+
+}
